08b0576e878c8eb0bd0ecfaf67cc299947fdeb35
James-Y-Wong <jamesyeuwong@gmail.com>
2022-07-25 12:42:15 -0400

    fixed portfolio section to render using map

--- portfolio/src/components/pages/Portfolio.js
+++ portfolio/src/components/pages/Portfolio.js
@@ -1,207 +1,99 @@
 import React from "react";
+import gadgetGetter from "../../images/gadgetgetter-dashboard-screenshot.png";
+import soundRiver from "../../images/soundriver_search_result_screenshot.png";
+import weatherDashboard from "../../images/weather-dashboard-screenshot.png";
+import passwordGenerator from "../../images/password-generator-password-result-screenshot.png";
+import workDayScheduler from "../../images/work-day-scheduler-home-screenshot.png";
+import codeQuiz from "../../images/code-quiz-quiz-page-screenshot.png";
+
+const projectArray = [
+  {
+    name: "GadgetGetter",
+    image: gadgetGetter,
+    deployedUrl: "https://gadget-getter.herokuapp.com/login",
+    repoUrl: "https://github.com/James-Y-Wong/GadgetGetter",
+    altAttribute: "GadgetGetter screenshot",
+  },
+  {
+    name: "SoundRiver",
+    image: soundRiver,
+    deployedUrl: "https://james-y-wong.github.io/SoundRiver/",
+    repoUrl: "https://github.com/James-Y-Wong/SoundRiver",
+    altAttribute: "SoundRiver screenshot",
+  },
+  {
+    name: "Weather Dashboard",
+    image: weatherDashboard,
+    deployedUrl: "https://james-y-wong.github.io/hw-6-weather-dashboard/",
+    repoUrl: "https://github.com/James-Y-Wong/hw-6-weather-dashboard",
+    altAttribute: "Weather Dashboard URL",
+  },
+  {
+    name: "Password Generator",
+    image: passwordGenerator,
+    deployedUrl: "https://james-y-wong.github.io/hw-3-password-generator/",
+    repoUrl: "https://github.com/James-Y-Wong/hw-3-password-generator",
+    altAttribute: "Password Generator screenshot",
+  },
+  {
+    name: "Work Day Scheduler",
+    image: workDayScheduler,
+    deployedUrl: "https://james-y-wong.github.io/hw-5-work-day-scheduler/",
+    repoUrl: "https://github.com/James-Y-Wong/hw-5-work-day-scheduler",
+    altAttribute: "Work Day Scheduler screenshot",
+  },
+  {
+    name: "Code Quiz",
+    image: codeQuiz,
+    deployedUrl: "https://james-y-wong.github.io/hw-4-code-quiz/",
+    repoUrl: "https://github.com/James-Y-Wong/hw-4-code-quiz",
+    altAttribute: "Code Quiz screenshot",
+  },
+];
 
 export default function Portfolio() {
   return (
-    <div className
-    ="container work-section">
-      <div className
-      ="row">
-        <div className
-        ="col-3">
+    <div className="container work-section">
+      <div className="row">
+        <div className="col-3">
           <h2 id="work">Portfolio</h2>
         </div>
-        <div className
-        ="col" id="project-cards">
-          <div className
-          ="col mb-4">
-            <div className
-            ="card">
-              <img
-                src="./assets/images/soundriver_search_result_screenshot.png"
-                className="card-img-top"
-                alt="screenshot of soundriver"
-              />
-              <div className
-              ="card-body">
-                <h5 className
-                ="card-title">SoundRiver</h5>
-                <p className
-                ="card-text">
-                  A web application that allows users to consolidate multiple
-                  streaming platforms into one playlist
-                </p>
-                <button title="URL">
-                  <a
-                    href="https://james-y-wong.github.io/SoundRiver/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className
-                    ="fa-solid fa-link"></i>
-                  </a>
-                </button>
-                <button title="GitHub Repo">
-                  <a
-                    href="https://github.com/James-Y-Wong/SoundRiver"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <i className
-                    ="fa-brands fa-github"></i>
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className
-          ="row row-cols-1 row-cols-md-2 g-4">
-            <div className
-            ="col">
-              <div className
-              ="card">
-                <img
-                  src="./assets/images/weather-dashboard-screenshot.png"
-                  className="card-img-top"
-                  alt="screenshot of weather dashboard"
-                />
-                <div className
-                ="card-body">
-                  <h5 className
-                  ="card-title">Weather Dashboard</h5>
-                  <p className
-                  ="card-text">
-                    A web application that allows user to see current and 5-day
-                    weather conditions for selected city
-                  </p>
-                  <button title="URL">
-                    <a
-                      href="https://james-y-wong.github.io/hw-6-weather-dashboard/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className
-                      ="fa-solid fa-link"></i>
-                    </a>
-                  </button>
-                  <button title="GitHub Repo">
-                    <a
-                      href="https://github.com/James-Y-Wong/hw-6-weather-dashboard"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className
-                      ="fa-brands fa-github"></i>
-                    </a>
-                  </button>
+        <div className="col" id="project-cards">
+          <div className="row row-cols-1 row-cols-md-2 g-4">
+            
+              {projectArray.map((project, index) => (
+                <div className="col">
+                <div className="card">
+                  <img
+                    src={project.image}
+                    className="card-img-top"
+                    alt={project.altAttribute}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{project.name}</h5>
+                    <button title="URL">
+                      <a
+                        href={project.deployedUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fa-solid fa-link"></i>
+                      </a>
+                    </button>
+                    <button title="GitHub Repo">
+                      <a
+                        href={project.repoUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <i className="fa-brands fa-github"></i>
+                      </a>
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className
-            ="col">
-              <div className
-              ="card">
-                <img
-                  src="./assets/images/gadgetgetter-dashboard-screenshot.png"
-                  className
-                  ="card-img-top"
-                  alt="screenshot of workday scheduler"
-                />
-                <div className
-                ="card-body">
-                  <h5 className="card-title">GadgetGetter</h5>
-                  <p className="card-text">
-                    A full-stack system for managing devices for a school. Can
-                    view allower users to rent and return devices
-                  </p>
-                  <button title="URL">
-                    <a
-                      href="https://gadget-getter.herokuapp.com/login"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-solid fa-link"></i>
-                    </a>
-                  </button>
-                  <button title="GitHub Repo">
-                    <a
-                      href="https://github.com/James-Y-Wong/GadgetGetter"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-brands fa-github"></i>
-                    </a>
-                  </button>
                 </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="./assets/images/code-quiz-quiz-page-screenshot.png"
-                  className="card-img-top"
-                  alt="screenshot of code quiz"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Code Quiz</h5>
-                  <p className="card-text">
-                    A web application to test users on basic coding knowledge
-                    using multiple choice questions
-                  </p>
-                  <button title="URL">
-                    <a
-                      href="https://james-y-wong.github.io/hw-4-code-quiz/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-solid fa-link"></i>
-                    </a>
-                  </button>
-                  <button title="GitHub Repo">
-                    <a
-                      href="https://github.com/James-Y-Wong/hw-4-code-quiz"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-brands fa-github"></i>
-                    </a>
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="./assets/images/work-day-scheduler-home-screenshot.png"
-                  className="card-img-top"
-                  alt="screenshot of password generator"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Workday Scheduler</h5>
-                  <p className="card-text">
-                    A web application to help user plan out tasks during the
-                    work day
-                  </p>
-                  <button title="URL">
-                    <a
-                      href="https://james-y-wong.github.io/hw-5-work-day-scheduler/"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-solid fa-link"></i>
-                    </a>
-                  </button>
-                  <button title="GitHub Repo">
-                    <a
-                      href="https://github.com/James-Y-Wong/hw-5-work-day-scheduler"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <i className="fa-brands fa-github"></i>
-                    </a>
-                  </button>
-                </div>
-              </div>
-            </div>
+              ))}
+            
           </div>
         </div>
       </div>
